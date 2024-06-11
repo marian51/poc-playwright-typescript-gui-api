78d2bf1ce394467bcbdbe140f58865bf81bdee60
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-11 15:20:06 +0200

    feat: add new test file with new test
    Added test for checking if creating new space works correct

---
+++ tests/space-basic-tests.spec.ts
@@ -0,0 +1,25 @@
+import test from "@playwright/test";
+import { LeftMenu } from "../page-objects/leftMenu";
+import { CreateSpaceModal } from "../page-objects/modals/createSpaceModal";
+
+test.only(
+    "Basic test for checking if creating new space works correct",
+    {
+        tag: "@space",
+    },
+    async ({ page }) => {
+        const leftMenu = new LeftMenu(page)
+        const createSpaceModal = new CreateSpaceModal(page)
+        const newSpaceName = "GUI TEST new space"
+
+        await page.goto("/")
+
+        await leftMenu.clickOnElement("Create Space")
+        await createSpaceModal.typeSpaceName(newSpaceName)
+        await createSpaceModal.clickOnContinueButton()
+        await createSpaceModal.clickOnButton("Create Space")
+        await leftMenu.assertElementIsVisible(newSpaceName)
+
+        await page.pause()
+    }
+);
