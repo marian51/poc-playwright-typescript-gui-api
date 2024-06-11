88fd59d8f336fead085384bc96a52294359e33c7
Pawel Bialczak <pawelnwz@gmail.com>
2024-06-11 13:37:14 +0200

    cleanup

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('first test', () => {
-  test.only('first test', async ({ page }) => {
-    await page.goto('https://app.clickup.com/')
-
-    // console.log(await page.context().storageState())
+  test('first test', async ({ page }) => {
+    await page.goto('/')
 
     await expect(page).toHaveTitle('Project 1 | Base Workspace (List)')
   })
