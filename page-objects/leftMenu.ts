587e583bddb26f1b5f277d2b7e882156139d5f20
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-11 15:19:38 +0200

    feat: add page class for 'Left menu' with basic content

---
+++ page-objects/leftMenu.ts
@@ -0,0 +1,25 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+export class LeftMenu {
+    private readonly page: Page;
+    private readonly leftSideBar: Locator;
+
+    private menuElement: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.leftSideBar = this.page.locator("cu-simple-bar");
+    }
+
+    clickOnElement = async (elementName: string) => {
+        this.menuElement = this.leftSideBar.getByRole('treeitem', { name: elementName })
+        await this.menuElement.waitFor()
+        await this.menuElement.click()
+    };
+
+    assertElementIsVisible = async (elementName:string) => {
+        this.menuElement = this.leftSideBar.getByRole('treeitem', { name: elementName })
+        await this.menuElement.waitFor()
+        await expect(this.menuElement).toBeVisible()
+    }
+}
