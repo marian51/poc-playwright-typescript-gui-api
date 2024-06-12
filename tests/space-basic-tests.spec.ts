db52c7e5aef6be195c11eaebbbadff3aa8727264
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-12 11:28:29 +0200

    feat: add deleting space after test

--- tests/space-basic-tests.spec.ts
+++ tests/space-basic-tests.spec.ts
@@ -1,25 +1,26 @@
 import test from "@playwright/test";
 import { LeftMenu } from "../page-objects/leftMenu";
 import { CreateSpaceModal } from "../page-objects/modals/createSpaceModal";
+import { ApiHooks } from "../api-utils/apiHooks";
 
 test.only(
-    "Basic test for checking if creating new space works correct",
-    {
-        tag: "@space",
-    },
-    async ({ page }) => {
-        const leftMenu = new LeftMenu(page)
-        const createSpaceModal = new CreateSpaceModal(page)
-        const newSpaceName = "GUI TEST new space"
+  "Basic test for checking if creating new space works correct",
+  {
+    tag: "@space",
+  },
+  async ({ page, request }) => {
+    const leftMenu = new LeftMenu(page)
+    const createSpaceModal = new CreateSpaceModal(page)
+    const newSpaceName = "GUI TEST new space"
 
-        await page.goto("/")
+    await page.goto("/")
 
-        await leftMenu.clickOnElement("Create Space")
-        await createSpaceModal.typeSpaceName(newSpaceName)
-        await createSpaceModal.clickOnContinueButton()
-        await createSpaceModal.clickOnButton("Create Space")
-        await leftMenu.assertElementIsVisible(newSpaceName)
+    await leftMenu.clickOnElement("Create Space")
+    await createSpaceModal.typeSpaceName(newSpaceName)
+    await createSpaceModal.clickOnContinueButton()
+    await createSpaceModal.clickOnButton("Create Space")
+    await leftMenu.assertElementIsVisible(newSpaceName)
 
-        await page.pause()
-    }
+    await ApiHooks.deleteSpaceByName(request, newSpaceName)
+  }
 );
