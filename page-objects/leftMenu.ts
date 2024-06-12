f76b323ef46f8820db6d92dacc14f973fc8111ce
Paweł Białczak <64209724+2pavel@users.noreply.github.com>
2024-06-12 11:59:22 +0200

    Added missing space

--- page-objects/leftMenu.ts
+++ page-objects/leftMenu.ts
@@ -17,7 +17,7 @@ export class LeftMenu {
         await this.menuElement.click()
     };
 
-    assertElementIsVisible = async (elementName:string) => {
+    assertElementIsVisible = async (elementName: string) => {
         this.menuElement = this.leftSideBar.getByRole('treeitem', { name: elementName })
         await this.menuElement.waitFor()
         await expect(this.menuElement).toBeVisible()
