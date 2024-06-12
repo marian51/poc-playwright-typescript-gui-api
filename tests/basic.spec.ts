80a1a263814f01a1e1b818e878dcda6ae432475a
Pawel Bialczak <pawelnwz@gmail.com>
2024-06-12 14:12:48 +0200

    Merged main into dev/auth

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('first test', () => {
-  test.only('first test', async ({ page }) => {
+  test('first test', async ({ page }) => {
     await page.goto('/');
 
     await expect(page).toHaveTitle('Project 1 | Base Workspace (List)');
