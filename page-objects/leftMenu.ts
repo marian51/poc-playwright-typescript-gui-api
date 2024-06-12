6ed5a6055542ad7062bd92f007331d778edae423
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-12 12:36:15 +0200

    refactor: run prettier with files
    Edited files: 'leftMenu.ts', 'createSpaceModal.ts', 'spaceBasicTests.spec.ts'

--- page-objects/leftMenu.ts
+++ page-objects/leftMenu.ts
@@ -1,25 +1,25 @@
 import { Locator, Page, expect } from "@playwright/test";
 
 export class LeftMenu {
-    private readonly page: Page;
-    private readonly leftSideBar: Locator;
+  private readonly page: Page;
+  private readonly leftSideBar: Locator;
 
-    private menuElement: Locator;
+  private menuElement: Locator;
 
-    constructor(page: Page) {
-        this.page = page;
-        this.leftSideBar = this.page.locator("cu-simple-bar");
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.leftSideBar = this.page.locator("cu-simple-bar");
+  }
 
-    async clickOnElement (elementName: string) {
-        this.menuElement = this.leftSideBar.getByRole('treeitem', { name: elementName })
-        await this.menuElement.waitFor()
-        await this.menuElement.click()
-    };
+  async clickOnElement(elementName: string) {
+    this.menuElement = this.leftSideBar.getByRole("treeitem", { name: elementName });
+    await this.menuElement.waitFor();
+    await this.menuElement.click();
+  }
 
-    async assertElementIsVisible (elementName: string) {
-        this.menuElement = this.leftSideBar.getByRole('treeitem', { name: elementName })
-        await this.menuElement.waitFor()
-        await expect(this.menuElement).toBeVisible()
-    }
+  async assertElementIsVisible(elementName: string) {
+    this.menuElement = this.leftSideBar.getByRole("treeitem", { name: elementName });
+    await this.menuElement.waitFor();
+    await expect(this.menuElement).toBeVisible();
+  }
 }
--- tests/spaceBasicTests.spec.ts
+++ tests/spaceBasicTests.spec.ts
@@ -3,21 +3,21 @@ import { LeftMenu } from "../page-objects/leftMenu";
 import { CreateSpaceModal } from "../page-objects/modals/createSpaceModal";
 
 test(
-    "Basic test for checking if creating new space works correct",
-    {
-        tag: "@space",
-    },
-    async ({ page }) => {
-        const leftMenu = new LeftMenu(page)
-        const createSpaceModal = new CreateSpaceModal(page)
-        const newSpaceName = "GUI TEST new space"
+  "Basic test for checking if creating new space works correct",
+  {
+    tag: "@space",
+  },
+  async ({ page }) => {
+    const leftMenu = new LeftMenu(page);
+    const createSpaceModal = new CreateSpaceModal(page);
+    const newSpaceName = "GUI TEST new space";
 
-        await page.goto("/")
+    await page.goto("/");
 
-        await leftMenu.clickOnElement("Create Space")
-        await createSpaceModal.typeSpaceName(newSpaceName)
-        await createSpaceModal.clickOnContinueButton()
-        await createSpaceModal.clickOnButton("Create Space")
-        await leftMenu.assertElementIsVisible(newSpaceName)
-    }
+    await leftMenu.clickOnElement("Create Space");
+    await createSpaceModal.typeSpaceName(newSpaceName);
+    await createSpaceModal.clickOnContinueButton();
+    await createSpaceModal.clickOnButton("Create Space");
+    await leftMenu.assertElementIsVisible(newSpaceName);
+  }
 );
