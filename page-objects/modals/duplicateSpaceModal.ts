20c0a884e78d89088bcb1b3c85346d296a2088f3
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-17 11:20:05 +0200

    feat: add class for 'Duplicate space' modal window
    Issue #48 reference

---
+++ page-objects/modals/duplicateSpaceModal.ts
@@ -0,0 +1,25 @@
+import { Locator, Page, expect } from "@playwright/test";
+
+export class DuplicateSpaceModal {
+  private readonly page: Page;
+  private readonly modalContainer: Locator;
+  private readonly nameInput: Locator;
+  private readonly duplicateButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.modalContainer = this.page.getByTestId("modal__dialog");
+    this.nameInput = this.modalContainer.getByTestId("form-field__box").getByRole("textbox");
+    this.duplicateButton = this.modalContainer.getByRole("button", { name: "Duplicate" });
+  }
+
+  async typeSpaceName(newSpaceName: string) {
+    await this.nameInput.waitFor();
+    await this.nameInput.fill(newSpaceName);
+  }
+
+  async clickOnDuplicateButton() {
+    await this.duplicateButton.waitFor();
+    await this.duplicateButton.click();
+  }
+}
