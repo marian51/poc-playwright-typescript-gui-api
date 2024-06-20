ba4385834d92f308c741bb54384f934fd445f263
Pawel Bialczak <pawelnwz@gmail.com>
2024-06-20 16:39:50 +0200

    added requests for goal feature testing

--- tests/api/basicApi.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-test.only("API test example", async ({ request }) => {
-  const response = await request.get(`/api/v2/team/${process.env.BASE_TEAM_ID}/goal`);
-  expect(response).toBeOK();
-});
---
+++ tests/api/goal.spec.ts
@@ -0,0 +1,44 @@
+import { expect, test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
+
+test.describe(
+  "Goal feature tests",
+  {
+    tag: ["@API", "@Goal"],
+  },
+  () => {
+    test("API test example", async ({ request }) => {
+      const response = await request.get(`/api/v2/team/${process.env.BASE_TEAM_ID}/goal`);
+      expect(response).toBeOK();
+    });
+
+    test("Create new goal", async ({ request }) => {
+      const newGoalBody = {
+        name: faker.person.jobType(),
+        due_date: 1568036964079,
+        description: faker.hacker.phrase(),
+        multiple_owners: false,
+        owners: [74498810],
+        color: faker.color.rgb(),
+      };
+      const response = await request.post(`/api/v2/team/${process.env.BASE_TEAM_ID}/goal`, { data: newGoalBody });
+      expect(response).toBeOK();
+    });
+
+    test.skip("Update existing goal", async ({ request }) => {
+      const goalId = "GOAL_ID"; // TODO: handle IDs
+      const updatedGoalBody = {
+        name: faker.animal.bear(),
+        color: "#000000",
+      };
+      const response = await request.put(`/api/v2/goal/${goalId}`, { data: updatedGoalBody });
+      expect(response).toBeOK();
+    });
+
+    test.skip("Delete a goal", async ({ request }) => {
+      const goalId = "GOAL_ID"; // TODO: handle IDs
+      const response = await request.delete(`/api/v2/goal/${goalId}`);
+      expect(response.status()).toEqual(200);
+    });
+  }
+);
