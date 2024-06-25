ab3c514936fbc5053ca58e6084e37b99dacfd832
Mariusz Jóźwiak <mariusz.jozwiak@int.pl>
2024-06-25 12:41:28 +0200

    refactor: rewrite structure of 'Doc' test file
    - Divided tests into 2 'describe' blocks: for tests of create/delete doc and for tests of changing existing doc
    - moved 'leftPage' variable up to 'beforeEach' hook
    - moved creating doc and going to website to 'beforeEach' hook in second 'describe' block
    - moved deleting doc to 'afterEach' hook in second 'describe' block
    - small changes in variables of doc name/title

--- tests/GUI/docBasicTests.spec.ts
+++ tests/GUI/docBasicTests.spec.ts
@@ -19,7 +19,8 @@ test.describe(
   },
   async () => {
     const newSpaceName = "GUI TEST new space";
-    const newDocName = faker.commerce.productName();
+    let leftMenu: LeftMenu;
+    let newDocName = faker.commerce.productName();
 
     test.beforeAll("Prepare environment before tests", async ({ request }) => {
       await ApiHooks.createSpaceByName(request, newSpaceName);
@@ -29,178 +30,165 @@ test.describe(
       await ApiHooks.deleteSpaceByName(request, newSpaceName);
     });
 
-    test(
-      "Basic test for creating new doc in existing space",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
-        },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const spaceContextMenu = new SpaceContextMenu(page);
-        const spacePlusMenu = new SpacePlusMenu(page);
-        const docView = new DocView(page);
+    test.beforeEach(async ({ page }) => {
+      leftMenu = new LeftMenu(page)
+    })
 
+    test.describe("Tests creating or deleting doc", async () => {
+      test(
+        "Basic test for creating new doc in existing space",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
+        },
+        async ({ page, request }) => {
+          const spaceContextMenu = new SpaceContextMenu(page);
+          const spacePlusMenu = new SpacePlusMenu(page);
+          const docView = new DocView(page);
+
+          await page.goto("/");
+          await page.locator("cu-web-push-notification-banner").waitFor();
+
+          await leftMenu.rightClickOnElement(newSpaceName);
+          await spaceContextMenu.clickOnOption("Create new");
+          await spacePlusMenu.clickOnOption("Doc");
+          await docView.typeDocTitle(newDocName);
+          await docView.clickKeyBoardKey("Enter");
+
+          await leftMenu.assertElementIsVisible(newDocName);
+
+          await ApiHooks.deleteDocsByName(request, newDocName);
+        }
+      );
+
+      test(
+        "Basic test for deleting existing doc in existing space",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
+        },
+        async ({ page, request }) => {
+          const docContextMenu = new DocContextMenu(page);
+
+          await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
+
+          await page.goto("/");
+          await page.locator("cu-web-push-notification-banner").waitFor();
+
+          await leftMenu.clickOnElement(newSpaceName);
+          await leftMenu.rightClickOnElement(newDocName);
+          await docContextMenu.clickOnOption("Delete");
+
+          await leftMenu.assertElementIsNotVisible(newDocName);
+        }
+      );
+    });
+
+    test.describe("Tests running on existing doc", async () => {
+      test.beforeEach("Prepare doc for test and go to website", async ({ page, request }) => {
+        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
         await page.goto("/");
         await page.locator("cu-web-push-notification-banner").waitFor();
+      });
 
-        await leftMenu.rightClickOnElement(newSpaceName);
-        await spaceContextMenu.clickOnOption("Create new");
-        await spacePlusMenu.clickOnOption("Doc");
-        await docView.typeDocTitle(newDocName);
-        await docView.clickKeyBoardKey("Enter");
-
-        await leftMenu.assertElementIsVisible(newDocName);
-
+      test.afterEach("Remove doc after test", async ({ request }) => {
         await ApiHooks.deleteDocsByName(request, newDocName);
-      }
-    );
+      });
 
-    test(
-      "Basic test for deleting existing doc in existing space",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+      test(
+        "Basic test for renaming existing doc in existing space",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
         },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const docContextMenu = new DocContextMenu(page);
+        async ({ page }) => {
+          const docContextMenu = new DocContextMenu(page);
+          const renamedDocName = faker.commerce.productName();
 
-        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
+          await leftMenu.clickOnElement(newSpaceName);
+          await leftMenu.rightClickOnElement(newDocName);
+          await docContextMenu.clickOnOption("Rename");
+          await leftMenu.typeIntoRenameDocInput(renamedDocName);
+          await leftMenu.clickKeyBoardKey("Enter");
 
-        await page.goto("/");
-        await page.locator("cu-web-push-notification-banner").waitFor();
+          await leftMenu.assertElementIsVisible(renamedDocName);
+          newDocName = renamedDocName;
+        }
+      );
 
-        await leftMenu.clickOnElement(newSpaceName);
-        await leftMenu.rightClickOnElement(newDocName);
-        await docContextMenu.clickOnOption("Delete");
-
-        await leftMenu.assertElementIsNotVisible(newDocName);
-      }
-    );
-
-    test(
-      "Basic test for renaming existing doc in existing space",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+      test(
+        "Test for checking if changing doc title changes doc name",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
         },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const docContextMenu = new DocContextMenu(page);
-        const renamedDocName = faker.commerce.productName();
+        async ({ page }) => {
+          const docView = new DocView(page);
+          const renamedDocTitle = faker.commerce.productName();
 
-        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
+          await leftMenu.clickOnElement(newSpaceName);
+          await page.getByRole("button", { name: "Add List" }).waitFor();
+          await leftMenu.clickOnElement(newDocName);
+          await docView.typeDocTitle(renamedDocTitle);
+          await docView.clickKeyBoardKey("Enter");
 
-        await page.goto("/");
-        await page.locator("cu-web-push-notification-banner").waitFor();
+          await leftMenu.assertElementIsVisible(renamedDocTitle);
+          await leftMenu.assertElementIsNotVisible(newDocName);
+          newDocName = renamedDocTitle;
+        }
+      );
 
-        await leftMenu.clickOnElement(newSpaceName);
-        await leftMenu.rightClickOnElement(newDocName);
-        await docContextMenu.clickOnOption("Rename");
-        await leftMenu.typeIntoRenameDocInput(renamedDocName);
-        await leftMenu.clickKeyBoardKey("Enter");
-
-        await leftMenu.assertElementIsVisible(renamedDocName);
-
-        await ApiHooks.deleteDocsByName(request, renamedDocName);
-      }
-    );
-
-    test(
-      "Test for checking if changing doc title changes doc name",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+      test(
+        "Basic test for creating new doc with the same name",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
         },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const docView = new DocView(page);
-        const renamedDocTitle = faker.commerce.productName();
+        async ({ page }) => {
+          const spaceContextMenu = new SpaceContextMenu(page);
+          const spacePlusMenu = new SpacePlusMenu(page);
+          const docView = new DocView(page);
 
-        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
+          await leftMenu.rightClickOnElement(newSpaceName);
+          await spaceContextMenu.clickOnOption("Create new");
+          await spacePlusMenu.clickOnOption("Doc");
+          await docView.typeDocTitle(newDocName);
+          await docView.clickKeyBoardKey("Enter");
 
-        await page.goto("/");
-        await page.locator("cu-web-push-notification-banner").waitFor();
+          await leftMenu.assertVisibleElementsNumber(newDocName, 2);
+        }
+      );
 
-        await leftMenu.clickOnElement(newSpaceName);
-        await page.getByRole("button", { name: "Add List" }).waitFor();
-        await leftMenu.clickOnElement(newDocName);
-        await docView.typeDocTitle(renamedDocTitle);
-        await docView.clickKeyBoardKey("Enter");
-
-        await leftMenu.assertElementIsVisible(renamedDocTitle);
-        await leftMenu.assertElementIsNotVisible(newDocName);
-
-        await ApiHooks.deleteDocsByName(request, renamedDocTitle);
-      }
-    );
-
-    test(
-      "Basic test for creating new doc with the same name",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+      test(
+        "Basic test for duplicating existing doc in existing space",
+        {
+          annotation: {
+            type: "issue",
+            description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
+          },
         },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const spaceContextMenu = new SpaceContextMenu(page);
-        const spacePlusMenu = new SpacePlusMenu(page);
-        const docView = new DocView(page);
+        async ({ page, request }) => {
+          const docContextMenu = new DocContextMenu(page);
 
-        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
+          await leftMenu.clickOnElement(newSpaceName);
+          await leftMenu.rightClickOnElement(newDocName);
+          await docContextMenu.clickOnOption("Duplicate");
 
-        await page.goto("/");
-        await page.locator("cu-web-push-notification-banner").waitFor();
+          await leftMenu.assertElementIsVisible(newDocName + " (copy)");
 
-        await leftMenu.rightClickOnElement(newSpaceName);
-        await spaceContextMenu.clickOnOption("Create new");
-        await spacePlusMenu.clickOnOption("Doc");
-        await docView.typeDocTitle(newDocName);
-        await docView.clickKeyBoardKey("Enter");
-
-        await leftMenu.assertElementsAreVisible(newDocName, 2);
-
-        await ApiHooks.deleteDocsByName(request, newDocName);
-      }
-    );
-
-    test(
-      "Basic test for duplicating existing doc in existing space",
-      {
-        annotation: {
-          type: "issue",
-          description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/68",
-        },
-      },
-      async ({ page, request }) => {
-        const leftMenu = new LeftMenu(page);
-        const docContextMenu = new DocContextMenu(page);
-
-        await ApiHooks.createNewDocInSpace(request, newDocName, await ApiUtils.getSpaceIdByName(request, newSpaceName));
-
-        await page.goto("/");
-        await page.locator("cu-web-push-notification-banner").waitFor();
-
-        await leftMenu.clickOnElement(newSpaceName);
-        await leftMenu.rightClickOnElement(newDocName);
-        await docContextMenu.clickOnOption("Duplicate");
-
-        await leftMenu.assertElementIsVisible(newDocName + " (copy)");
-
-        await ApiHooks.deleteDocsByName(request, newDocName);
-        await ApiHooks.deleteDocsByName(request, newDocName + " (copy)");
-      }
-    );
+          await ApiHooks.deleteDocsByName(request, newDocName + " (copy)");
+        }
+      );
+    });
   }
 );
