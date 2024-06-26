f0bc0aaf4073f247f10dc3cfc627da35cd867377
Maciej <maciek6g@gmail.com>
2024-06-26 13:53:45 +0200

    added beforeEach to folder tests

--- tests/GUI/folder.spec.ts
+++ tests/GUI/folder.spec.ts
@@ -9,14 +9,16 @@ test.describe(
   "Folder tests",
   { tag: "@folder", annotation: { type: "issue", description: "https://github.com/marian51/poc-playwright-typescript-gui-api/issues/87" } },
   () => {
+    const newFolderName = "GUI TEST folder";
+    let leftMenu: LeftMenu;
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/");
+      leftMenu = new LeftMenu(page);
+    });
     test("Test for checking if creating new folder works correctly", async ({ page }) => {
       const spaceName = "Team Space";
-      const newFolderName = "GUI TEST folder";
-      const leftMenu = new LeftMenu(page);
       const spaceCreateContextMenu = new SpaceCreateContextMenu(page);
       const createFolderModal = new CreateFolderModal(page);
-
-      await page.goto("/");
 
       await leftMenu.clickOnSpacePlusButton(spaceName);
       await spaceCreateContextMenu.clickOnOption("category");
@@ -26,12 +28,8 @@ test.describe(
     });
 
     test("Test for checking if deleting existing folder works correctly", async ({ page }) => {
-      const newFolderName = "GUI TEST folder";
-      const leftMenu = new LeftMenu(page);
       const folderContextMenu = new FolderContextMenu(page);
       const deleteFolderModal = new DeleteFolderModal(page);
-
-      await page.goto("/");
 
       await leftMenu.rightClickOnElement(newFolderName);
       await folderContextMenu.clickOnOption("Delete");
@@ -41,12 +39,8 @@ test.describe(
     });
 
     test("Test for checking if renaming existing folder works correctly", async ({ page }) => {
-      const newFolderName = "GUI TEST folder";
       const renamedFolderName = "Renamed GUI TEST folder";
-      const leftMenu = new LeftMenu(page);
       const folderContextMenu = new FolderContextMenu(page);
-
-      await page.goto("/");
 
       await leftMenu.rightClickOnElement(newFolderName);
       await folderContextMenu.clickOnOption("Rename");
