dc4025def362e88d61596b11f93c607b84ab86b9
Pawel Bialczak <pawelnwz@gmail.com>
2024-08-06 14:34:13 +0200

    reverted config trace setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     baseURL: "https://app.clickup.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on",//TODO: change back to retain-on-failure
+    trace: "retain-on-failure",
     video: "retain-on-failure",
 
     /* Change the attribute by which method 'page.getByTestId(...)' looks for elements. See https://playwright.dev/docs/locators#set-a-custom-test-id-attribute*/
